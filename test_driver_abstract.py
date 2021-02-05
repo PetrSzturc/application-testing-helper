db44db91a7a97572cdd9a780def3d1f4baf1802c
Petr Szturc <szt.pracovni1@gmail.com>
2021-02-05 23:33:46 +0000

    concept final

--- test_driver_abstract.py
+++ test_driver_abstract.py
@@ -1,126 +1,148 @@
 from appium.webdriver import Remote as appium_driver
-
 from selenium.webdriver import Chrome, Firefox
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webelement import WebElement
 
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
 import logging
+from time import sleep
+
+from pytest import fixture
 
 from configuration.configuration import setup_logging
 
-from selenium.webdriver.common.by import By
-from time import sleep
-from selenium.webdriver.common.keys import Keys
 
 setup_logging()
 log = logging.getLogger(__name__)
-
-
-log.info(f"TODO: cleanup driver.driver and similar https://app.clickup.com/t/dn4ea9")
 
 
 class Driver(object):
 
     def __init__(
         self, 
-        # driver_type
+        # driver_type,
         ):
+        # TODO Specifying drive_type via argument instead of calling firefox() might be better when called from terminal
         mapping = {
             "firefox": (Firefox, "browser"),
             "chrome": (Chrome, "browser"),
             "mobile": (appium_driver, "android"),
         }
-        # try:            
-            # eg return chrome instance or appium instance
-            # self.driver, self.locator_type = mapping[driver_type]
-            # self.driver_type = driver_type
-            # self.driver = self.driver()
-            # return driver()
-        # except KeyError:
-            # log.error(f"Tried to call driver {driver_type} which isn't configured.")
+        self.platform_driver = None
+        self.locator_type = None
 
     def firefox(self):
-        # move this here from init, so that I can call it like
-        # Driver().firefox()
-        self.locator_type = "browser_locator"
-        # self.driver_type = driver_type
-        # self.driver = self.driver()
-        self.driver = Firefox(executable_path=GeckoDriverManager().install())
-        # return self
+        self.locator_type = "browser"
+        # TODO setup of platforms should be moved elsewhere
+        self.platform_driver = Firefox(executable_path=GeckoDriverManager().install())
+        return self
 
-    def get(self, url):
-        return self.driver.get(url)
+    def go_to(self, url):
+        self.platform_driver.get(url)
 
     def quit(self):
-        self.driver.quit()
+        self.platform_driver.quit()
 
-    def find_element(self, locator):
-        pass
-        return self.driver.find_element(*getattr(locator, self.locator_type))
+    def find_element(self, app_element) -> WebElement:
+        # Select appropriate locator and pass it based on running driver->locator type
+        if isinstance(app_element, AppElement):
+            return self.platform_driver.find_element(*getattr(app_element, self.locator_type))
+        return self.platform_driver.find_element(*app_element)
         
 
+class AppUi(object):
+
+    def __init__(self, driver: Driver):
+        self.driver = driver
+        self.home_screen = HomeScreen(self)
+
+
+class BaseScreen(object):
+    def __init__(self, app: AppUi):
+        self.driver = app.driver
+        
 
 class AppElement(object):
 
     def __init__(self,
-                 browser_locator = None,
-                 android_locator = None,
-                 ios_locator = None
+                 browser = None,
+                 android = None,
+                 ios = None
                  ):
-        self.browser_locator = browser_locator
-        self.android_locator = android_locator
-        self.ios_locator = ios_locator
+        self.browser = browser
+        self.android = android
+        self.ios = ios
 
-    def __get__(self, instance, owner):
-        locators_resolution = {
-            Chrome: self.browser_locator,
-            Firefox: self.browser_locator,
-            # Android: self.android_locator,
-            # Ios: self.ios_locator,
-        }
+    def __get__(self, instance: BaseScreen, owner) -> WebElement:
+        # Leave the platform resolution to Driver().
         return instance.driver.find_element(self)
 
 
-class BaseSCreen(object):
-    def __init__(self, object_with_driver):
-        self.driver = object_with_driver.driver
-
-
-class HomeScreen(BaseSCreen):
+class HomeScreen(BaseScreen):
     SEARCH_FIELD = AppElement(
-        browser_locator=(By.CSS_SELECTOR, 'div.search-form input[name=q].input')
+        browser=(By.CSS_SELECTOR, 'div.search-form input[name=q].input'),
         )
 
     def search(self, term):
-        # log.info(f"HomeScreen.SEARCH_FIELD: {HomeScreen.SEARCH_FIELD}")
-        # search_field = self.driver.find_element(HomeScreen.SEARCH_FIELD)
-        # search_field.send_keys("chata")
-        # search_field.send_keys(Keys.ENTER)
+        assert self.SEARCH_FIELD.is_displayed()
         self.SEARCH_FIELD.send_keys("chata")
         self.SEARCH_FIELD.send_keys(Keys.ENTER)
-        sleep(2)
+        log.info(f"Searching for: {term}.")
+
+
+# Simplify usage for both test functions and test classes.
+def _setup() -> AppUi:
+    log.info(f"Test setup.")
+    driver = Driver().firefox()
+    app = AppUi(driver)
+    driver.go_to("https://seznam.cz")
+    return app
+
+def _teardown(app: AppUi):
+    # TODO sleep
+    sleep(2)
+
+    log.info(f"Test teardown.")
+    app.driver.quit()
 
 
 class BaseTest(object):
 
     def setup_method(self):
-        log.info(f"Setup")
-        self.driver = Driver()
-        self.driver.firefox()
-        self.home_screen = HomeScreen(self)
-
-        self.driver.get("https://seznam.cz")
+        self.app = _setup()
 
     def teardown_method(self):
-        sleep(2)
-        log.info("Closing")
-        self.driver.quit()
+        _teardown(self.app)
 
 
 class TestSearch(BaseTest):
 
     def test_simple_search(self):
-        log.info(f"Start test {__name__}")
-        self.home_screen.search("chata")
+        self.app.home_screen.search("chata")
+
+
+@fixture
+def app():
+    app = _setup()
+    yield app
+    _teardown(app)
+
+
+def test_simple_search(app: AppUi):
+    app.home_screen.search("chata")
+
+
+def test_with_adhoc_elements(app: AppUi):
+    search_field = app.driver.find_element((By.CSS_SELECTOR, 'div.search-form input[name=q].input'))
+    assert search_field.is_displayed()
+    search_field.send_keys("chata")
+    search_field.send_keys(Keys.ENTER)
+
+    
+def test_with_predefined_elements(app: AppUi):
+    assert app.home_screen.SEARCH_FIELD.is_displayed()
+    app.home_screen.SEARCH_FIELD.send_keys("chata")
+    app.home_screen.SEARCH_FIELD.send_keys(Keys.ENTER)
