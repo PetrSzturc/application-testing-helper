03d1534ed75bb3fe3c1fa7c30b425b1db2cd966e
Petr Szturc <szt.pracovni1@gmail.com>
2021-02-08 16:39:03 +0000

    feature: prototype working with all browsers

--- test_playwright.py
+++ test_playwright.py
@@ -1,8 +1,15 @@
 from playwright.sync_api import sync_playwright
+from time import sleep
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    # browser = p.chromium.launch(headless=False)
+    browser = p.firefox.launch(headless=False)
+    # browser = p.webkit.launch(headless=False)
     page = browser.new_page()
-    page.goto("http://playwright.dev")
+    page.goto("https://seznam.cz")
+    search_field = page.query_selector('div.search-form input[name=q].input')
+    search_field.fill("chata")
+    search_field.press('Enter')
+    sleep(3)
     print(page.title())
     browser.close()
